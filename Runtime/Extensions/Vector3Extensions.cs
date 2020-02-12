9711b50cc680899a1df07dd20e86009e54191ff5
Ed Casillas <eduardo_casillas@hotmail.com>
2020-02-12 16:03:14 -0600

    Included randomizable Vector3

--- Runtime/Extensions/Vector3Extensions.cs
+++ Runtime/Extensions/Vector3Extensions.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 namespace CommonUtils.Extensions {
 	public static class Vector3Extensions {
-		public static Vector2 GetAnglesTo(this Vector3 referenceVector, Vector3 compareVector) => new Vector2(
-			-Mathf.Asin(Vector3.Cross(compareVector, referenceVector).y) * Mathf.Rad2Deg,
-			-Mathf.Asin(Vector3.Cross(compareVector, referenceVector).x) * Mathf.Rad2Deg);
+		public static Vector2 GetAnglesTo(this Vector3 referenceVector, Vector3 compareVector)
+			=> new Vector2(-Mathf.Asin(Vector3.Cross(compareVector, referenceVector).y) * Mathf.Rad2Deg,
+						   -Mathf.Asin(Vector3.Cross(compareVector, referenceVector).x) * Mathf.Rad2Deg);
+
+		public static Vector3 RandomRange(Vector3 min, Vector3 max) => new Vector3(Random.Range(min.x, max.x),
+																				   Random.Range(min.y, max.y),
+																				   Random.Range(min.z, max.z));
 	}
 }
---
+++ Runtime/Randomizables/RandomVector3.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using CommonUtils.Extensions;
+using UnityEngine;
+
+namespace CommonUtils.Randomizables {
+	[Serializable]
+	public class RandomVector3 : AbstractRandomizable<Vector3> {
+		public override Vector3 Get() => Vector3Extensions.RandomRange(Min, Max);
+	}
+}
