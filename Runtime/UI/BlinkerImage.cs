4e919b5301fcbb1c2412dcb6e4083e0380518e2c
Ed Casillas <eduardo_casillas@hotmail.com>
2020-02-13 16:36:50 -0600

    Added a base blinker image component.

---
+++ Runtime/UI/BlinkerImage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CommonUtils.Extensions;
+using UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CommonUtils.UI {
+    [RequireComponent(typeof(Image))]
+    public class BlinkerImage : MonoBehaviour, IBlinkerUIElement {
+		#pragma warning disable 649
+        [SerializeField] private float blinkingRate;
+        [SerializeField] private bool autoStart;
+		#pragma warning restore 649
+
+        public float BlinkingRate => blinkingRate;
+
+        private IEnumerable<Graphic> graphics;
+
+        public IEnumerable<Graphic> BlinkerGraphics => graphics ?? (graphics = new[] {GetComponent<Image>()});
+
+        private IReadOnlyDictionary<Graphic, float> originalAlphaValues;
+
+        public IReadOnlyDictionary<Graphic, float> OriginalAlphaValues => originalAlphaValues ??
+                                                                          (originalAlphaValues = BlinkerGraphics.ToDictionary(g => g, g => g.color.a));
+
+		private void OnEnable() {
+			if(autoStart) this.StartBlinking();
+		}
+
+		private void OnDisable() {
+			if(autoStart) this.StopBlinking();
+		}
+	}
+}
