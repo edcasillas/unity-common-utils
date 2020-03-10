21f396002b7f4f6a42d048999dee403049175fc5
Ed Casillas <eduardo_casillas@hotmail.com>
2020-03-09 22:34:22 -0600

    Included utility to highlight assets in the hierarchy.

---
+++ Runtime/Scripts/EditorUtils.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace CommonUtils {
+    public static class EditorUtils {
+        public static void HighlightAssetOfType<TAsset>(string assetPath) where TAsset : Object {
+            #if UNITY_EDITOR
+            var instance = (TAsset) Resources.Load(assetPath, typeof(TAsset));
+
+            if (!instance) {
+                Debug.LogError($"Asset of type {typeof(TAsset)} could not be found at \"{assetPath}\"");
+                return;
+            }
+
+            UnityEditor.Selection.objects = new Object[] {instance};
+            UnityEditor.EditorGUIUtility.PingObject(instance);
+            #endif
+        }
+    }
+}
