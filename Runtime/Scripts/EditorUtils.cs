dfa7b334fb1f70d1c2bac3f66a58fd6c0e220752
Ed Casillas <eduardo_casillas@hotmail.com>
2020-03-09 22:51:32 -0600

    Added utility to save objects as JSON file.

--- Runtime/Scripts/EditorUtils.cs
+++ Runtime/Scripts/EditorUtils.cs
@@ -4,7 +4,7 @@ using Object = UnityEngine.Object;
 namespace CommonUtils {
     public static class EditorUtils {
         public static void HighlightAssetOfType<TAsset>(string assetPath) where TAsset : Object {
-            #if UNITY_EDITOR
+#if UNITY_EDITOR
             var instance = (TAsset) Resources.Load(assetPath, typeof(TAsset));
 
             if (!instance) {
@@ -14,7 +14,28 @@ namespace CommonUtils {
 
             UnityEditor.Selection.objects = new Object[] {instance};
             UnityEditor.EditorGUIUtility.PingObject(instance);
-            #endif
+#endif
+        }
+
+        public static bool SaveAsJsonFile<T>(this T source, string title = null) {
+            var path = UnityEditor.EditorUtility.SaveFilePanel($"Save {typeof(T)} as JSON",
+                                                               "",
+                                                               $"{typeof(T)}.json",
+                                                               "json");
+            if (!string.IsNullOrWhiteSpace(path)) {
+                var jsonContents = JsonUtility.ToJson(source);
+                try {
+                    System.IO.File.WriteAllText(path, jsonContents);
+                    Debug.Log($"{typeof(T)} was saved as JSON at {path}");
+                    return true;
+                } catch (System.Exception ex) {
+                    Debug.LogError($"Could not save the {typeof(T)} file: {ex.Message}");
+                }
+            } else {
+                Debug.Log("Save cancelled");
+            }
+
+            return false;
         }
     }
 }
