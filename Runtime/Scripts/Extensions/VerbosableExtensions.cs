b88c761c06f0bc89e051f3e12fdde64fe1de2937
Ed Casillas <eduardo_casillas@hotmail.com>
2020-03-19 13:50:43 -0600

    Added XML docs to verbosables.

--- Runtime/Scripts/Extensions/VerbosableExtensions.cs
+++ Runtime/Scripts/Extensions/VerbosableExtensions.cs
@@ -3,11 +3,25 @@ using UnityEngine;
 
 namespace CommonUtils.Extensions {
 	public static class VerbosableExtensions {
+		/// <summary>
+		/// Writes a <paramref name="message"/> to the console if the <paramref name="verbosable"/> has its <see cref="IVerbosable.IsVerbose"/> property set to true.
+		/// </summary>
+		/// <param name="verbosable">Verbosable component writing the message.</param>
+		/// <param name="message">Message to be sent to the console.</param>
+		/// <typeparam name="TVerbosable">Type of verbosable component.</typeparam>
 		public static void DebugLog<TVerbosable>(this TVerbosable verbosable, string message)
 			where TVerbosable : MonoBehaviour, IVerbosable {
 			if (verbosable.IsVerbose) Debug.Log(message, verbosable);
 		}
 		
+		/// <summary>
+		/// Writes a message to the console if the <paramref name="verbosable"/> has its <see cref="IVerbosable.IsVerbose"/> property set to true.
+		/// The <paramref name="messageDelegate"/> is only invoked when the message is effectively being sent to the console, so this variant of DebugLog is meant to
+		/// be used when constructing the message can be an expensive operation.
+		/// </summary>
+		/// <param name="verbosable">Verbosable component writing the message.</param>
+		/// <param name="messageDelegate">Function that creates the message to be sent to the console.</param>
+		/// <typeparam name="TVerbosable">Type of verbosable component.</typeparam>
 		public static void DebugLog<TVerbosable>(this TVerbosable verbosable, Func<string> messageDelegate)
 			where TVerbosable : MonoBehaviour, IVerbosable {
 			if (verbosable.IsVerbose) Debug.Log(messageDelegate.Invoke(), verbosable);
