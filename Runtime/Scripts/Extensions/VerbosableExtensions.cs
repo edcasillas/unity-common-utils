658e4533d95554aeb9294e13cb2ead1edf6ea021
Ed Casillas <eduardo_casillas@hotmail.com>
2020-03-25 20:04:33 -0600

    Continue improving extensions.

--- Runtime/Scripts/Extensions/VerbosableExtensions.cs
+++ Runtime/Scripts/Extensions/VerbosableExtensions.cs
@@ -14,7 +14,18 @@ namespace CommonUtils.Extensions {
 			where TVerbosable : Object, IVerbosable {
 			if (verbosable.IsVerbose) Debug.Log(message, verbosable);
 		}
-		
+
+		/// <summary>
+		/// Writes a <paramref name="message"/> to the console if the <paramref name="verbosable"/> has its <see cref="IVerbosable.IsVerbose"/> property set to true.
+		/// </summary>
+		/// <param name="verbosable">Verbosable component writing the message.</param>
+		/// <param name="message">Message to be sent to the console.</param>
+		/// <typeparam name="TVerbosable">Type of verbosable component.</typeparam>
+		public static void DebugLog2<TVerbosable>(this TVerbosable verbosable, string message) // TODO Find a way to resolve naming conflicts
+			where TVerbosable : IVerbosable, IUnityComponent {
+			if (verbosable.IsVerbose) Debug.Log(message, verbosable.gameObject);
+		}
+
 		/// <summary>
 		/// Writes a message to the console if the <paramref name="verbosable"/> has its <see cref="IVerbosable.IsVerbose"/> property set to true.
 		/// The <paramref name="messageDelegate"/> is only invoked when the message is effectively being sent to the console, so this variant of DebugLog is meant to
@@ -26,6 +37,19 @@ namespace CommonUtils.Extensions {
 		public static void DebugLog<TVerbosable>(this TVerbosable verbosable, Func<string> messageDelegate)
 			where TVerbosable : Object, IVerbosable {
 			if (verbosable.IsVerbose) Debug.Log(messageDelegate.Invoke(), verbosable);
+		}
+		
+		/// <summary>
+		/// Writes a message to the console if the <paramref name="verbosable"/> has its <see cref="IVerbosable.IsVerbose"/> property set to true.
+		/// The <paramref name="messageDelegate"/> is only invoked when the message is effectively being sent to the console, so this variant of DebugLog is meant to
+		/// be used when constructing the message can be an expensive operation.
+		/// </summary>
+		/// <param name="verbosable">Verbosable component writing the message.</param>
+		/// <param name="messageDelegate">Function that creates the message to be sent to the console.</param>
+		/// <typeparam name="TVerbosable">Type of verbosable component.</typeparam>
+		public static void DebugLog2<TVerbosable>(this TVerbosable verbosable, Func<string> messageDelegate)
+			where TVerbosable : IVerbosable, IUnityComponent {
+			if (verbosable.IsVerbose) Debug.Log(messageDelegate.Invoke(), verbosable.gameObject);
 		}
 	}
 }
