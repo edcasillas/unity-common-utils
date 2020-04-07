a02d7fe36523ce982b8b03edfd0629b7cb273c56
Ed Casillas <eduardo_casillas@hotmail.com>
2020-04-07 02:42:15 -0500

    Added PriorityQueue and DynamicPriorityQueue

---
+++ Runtime/Scripts/Heaps/DynamicPriorityQueue.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommonUtils.Heaps {
+	/// <summary>
+	/// Extends <see cref="PriorityQueue{T}"/> to enable changing the priority of
+	/// its elements and constant-time access to them.
+	/// </summary>
+	/// <typeparam name="T">Specifies the type of elements in the queue.</typeparam>
+	public class DynamicPriorityQueue<T> : PriorityQueue<T> where T : IComparable<T> {
+		private readonly Dictionary<T,int> indexOf = new Dictionary<T, int>();
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DynamicPriorityQueue{T}"/> class. An optional collection can be specified
+		/// as argument to copy its elements to the queue and ensure sufficient capacity to accommodate the number of
+		/// elements copied, but will throw an exception if elements are repeated.
+		/// </summary>
+		/// <param name="source">The collection whose elements are copied to the new <see cref="DynamicPriorityQueue{T}"/>.</param>
+		/// <exception cref="ArgumentException">An item in <paramref name="source"/> is repeated.</exception>
+		public DynamicPriorityQueue(IEnumerable<T> source = null) : base(source) {
+			for (var i = 0; i < Data.Count; i++) {
+				if (indexOf.ContainsKey(Data[i]))
+					throw new ArgumentException($"Elements in a {nameof(DynamicPriorityQueue<T>)} cannot be repeated.");
+
+				indexOf.Add(Data[i], i);
+			}
+		}
+
+		/// <summary>
+		/// Adds an object to the queue. If the object already exists, checks and fixes the priority of the queue.
+		/// </summary>
+		/// <param name="item">The object to add or update into the queue.</param>
+		public override void Enqueue(T item) {
+			if (!indexOf.ContainsKey(item)) {
+				indexOf.Add(item, Data.Count);
+				base.Enqueue(item);
+			}
+			else {
+				FixHeap(indexOf[item]);
+			}
+		}
+
+		/// <summary>
+		/// Removes and returns the object at the beginning of the queue.
+		/// </summary>
+		/// <returns>The object that is removed from the beginning of the queue.</returns>
+		public override T Dequeue() {
+			var result = base.Dequeue();
+			if (indexOf.ContainsKey(result)) {
+				indexOf.Remove(result);
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Determines whether an element is in the queue.
+		/// </summary>
+		/// <param name="item">The object to locate in the queue.</param>
+		/// <returns><c>true</c> if <paramref name="item"/> is found in the queue; othwewise, <c>false</c>.</returns>
+		public bool Contains(T item) => indexOf.ContainsKey(item);
+
+		/// <summary>
+		/// Removes the specified <paramref name="item"/> from the queue.
+		/// </summary>
+		/// <param name="item">The item to be removed.</param>
+		/// <exception cref="InvalidOperationException">The specified <paramref name="item"/> does not exists in the queue.</exception>
+		public void Remove(T item) {
+			if(!indexOf.ContainsKey(item)) throw new InvalidOperationException($"The specified value does not exist in this {nameof(DynamicPriorityQueue<T>)}");
+			RemoveAt(indexOf[item]);
+			indexOf.Remove(item);
+		}
+
+		protected override void Swap(int i, int j) {
+			base.Swap(i, j);
+
+			// After swapping, update the indices stored for each object.
+			indexOf[Data[i]] = i;
+			indexOf[Data[j]] = j;
+		}
+	}
+}
---
+++ Runtime/Scripts/Heaps/PriorityQueue.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommonUtils.Heaps {
+	/// <summary>
+	/// Implements a generic min-heap where the priority of its elements is given
+	/// by their implementation of the <see cref="IComparable{T}"/> interface.
+	/// </summary>
+	/// <typeparam name="T">Specifies the type of elements in the queue.</typeparam>
+	public class PriorityQueue<T> where T : IComparable<T> {
+		/// <summary>
+		/// Underlying structure holding the items of the heap.
+		/// </summary>
+		protected readonly List<T> Data;
+
+		/// <summary>
+		/// Gets the number of elements contained in the <see cref="PriorityQueue{T}"/>.
+		/// </summary>
+		public int Count => Data.Count;
+
+		/// <summary>
+		/// Gets a value indicating whether the <see cref="PriorityQueue{T}"/> is empty.
+		/// </summary>
+		public bool IsEmpty => Count == 0;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PriorityQueue{T}"/> class. An optional collection can be specified
+		/// as argument to copy its elements to the queue and ensure sufficient capacity to accommodate the number of
+		/// elements copied.
+		/// </summary>
+		/// <param name="source">The collection whose elements are copied to the new <see cref="PriorityQueue{T}"/>.</param>
+		public PriorityQueue(IEnumerable<T> source = null) {
+			Data = source == null ? new List<T>() : new List<T>(source);
+			for (var i = Data.Count / 2 - 1; i >= 0; i--) heapifyDown(i);
+		}
+
+		/// <summary>
+		/// Adds an object to the queue.
+		/// </summary>
+		/// <param name="item">The object to add to the queue.</param>
+		public virtual void Enqueue(T item) {
+			Data.Add(item);
+			heapifyUp(getParentIndex(Data.Count - 1));
+		}
+
+		/// <summary>
+		/// Removes and returns the object at the beginning of the queue.
+		/// </summary>
+		/// <returns>The object that is removed from the beginning of the queue.</returns>
+		public virtual T Dequeue() {
+			if (Data.Count == 0) return default;
+			var result = Data[0];
+
+			if (Data.Count == 1) {
+				Data.RemoveAt(0);
+			} else {
+				Data[0] = Data[Data.Count -1];
+				Data.RemoveAt(Data.Count - 1);
+				heapifyDown();
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Returns the object at the beginning of the queue without removing it.
+		/// </summary>
+		/// <returns></returns>
+		public T Peek() => Data.Count > 0 ? Data[0] : default;
+
+		/// <summary>
+		/// Removes an arbitrary item from the queue given its <paramref name="index"/> in the heap.
+		/// </summary>
+		/// <param name="index">Index of the item to remove.</param>
+		/// <exception cref="IndexOutOfRangeException">The specified <paramref name="index"/> is not valid.</exception>
+		public void RemoveAt(int index) {
+			if (Data.Count == 0) return;
+
+			if (index < 0 || index >= Data.Count) throw new IndexOutOfRangeException();
+
+			if (index == 0) {
+				Dequeue();
+				return;
+			}
+
+			Data[index] = Data[Data.Count -1];
+			Data.RemoveAt(Data.Count - 1);
+
+			FixHeap(index);
+		}
+
+		/// <summary>
+		/// Fixes the heap assuming a conflict at the specified <paramref name="index"/> of the heap.
+		/// If the priority of the item at <paramref name="index"/> is higher than its parent, bubbles up the item at <paramref name="index"/>,
+		/// otherwise bubbles it down.
+		/// </summary>
+		/// <param name="index">Index of the heap that must be fixed.</param>
+		protected void FixHeap(int index) {
+			if (index > 0 && Data[getParentIndex(index)].CompareTo(Data[index]) < 0) {
+				heapifyUp(getParentIndex(index));
+			} else {
+				heapifyDown(index);
+			}
+		}
+
+		/// <summary>
+		/// Swaps the contents of the two specified indices in the heap.
+		/// </summary>
+		protected virtual void Swap(int i, int j) {
+			var tmp = Data[i];
+			Data[i] = Data[j];
+			Data[j] = tmp;
+		}
+
+		private void heapifyUp(int i) {
+			if (i < 0 || i > Data.Count) return;
+			var left = getLeftChildIndex(i);
+			var right = getRightChildIndex(i);
+			var min = i;
+			if (left < Data.Count && Data[left].CompareTo(Data[min]) < 0) min = left;
+			if (right < Data.Count && Data[right].CompareTo(Data[min]) < 0) min = right;
+			if (min != i) {
+				Swap(min, i);
+				heapifyUp(getParentIndex(i));
+			}
+		}
+
+		private void heapifyDown(int i = 0) {
+			if (i < 0 || i > Data.Count) return;
+			var left = getLeftChildIndex(i);
+			var right = getRightChildIndex(i);
+			var min = i;
+			if (left < Data.Count && Data[left].CompareTo(Data[min]) < 0) min = left;
+			if (right < Data.Count && Data[right].CompareTo(Data[min]) < 0) min = right;
+			if (min != i) {
+				Swap(min, i);
+				heapifyDown(min);
+			}
+		}
+
+		private static int getLeftChildIndex(int i) => (2 * i) + 1;
+		private static int getRightChildIndex(int i) => (2 * i) + 2;
+		private static int getParentIndex(int i) => (i - 1) / 2;
+	}
+}
