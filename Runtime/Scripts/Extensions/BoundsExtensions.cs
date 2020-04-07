73e786b355a52afde7973c6bc15268a9118c0c7e
Ed Casillas <eduardo_casillas@hotmail.com>
2020-04-07 18:51:16 -0500

    Added some more extension methods.

---
+++ Runtime/Scripts/Extensions/BoundsExtensions.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace CommonUtils.Extensions {
+	public static class BoundsExtensions {
+		public static BoundingSphere ToBoundingSphere(this Bounds bounds) => new BoundingSphere {position = bounds.center, radius = bounds.extents.magnitude};
+	}
+}
