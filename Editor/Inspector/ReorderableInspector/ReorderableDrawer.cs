1587747cf3f05975cfd997587fe49fc0a2f88add
Ed Casillas <eduardo_casillas@hotmail.com>
2020-04-22 23:19:16 -0500

    First working version of my own reorderable drawer.

--- Editor/Inspector/ReorderableInspector/ReorderableDrawer.cs
+++ Editor/Inspector/ReorderableInspector/ReorderableDrawer.cs
@@ -6,89 +6,84 @@ using UnityEngine;
 namespace CommonUtils.Editor.Inspector.ReorderableInspector {
 	[CustomPropertyDrawer(typeof(ReorderableAttribute))]
 	public class ReorderableDrawer : PropertyDrawer {
-		private ReorderableList list;
-
 		public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
-			//initialize(property);
-			//return list.GetHeight();
-			return EditorGUI.GetPropertyHeight(property, label, property.isExpanded);
-		}
+			var i = getIndexInArray(property);
+			var parent = SerializedPropExtension.GetParentProp(property);
+			if(i >= parent.arraySize) Debug.LogError("HERE!!!");
 
-		public static float ElementHeightCallback(SerializedProperty property, int index) {
-			SerializedProperty arrayElement = property.GetArrayElementAtIndex(index);
-			float calculatedHeight = EditorGUI.GetPropertyHeight(arrayElement, GUIContent.none, arrayElement.isExpanded);
-			calculatedHeight += 3;
-			return calculatedHeight;
+			try {
+				return EditorGUI.GetPropertyHeight(property, label, property.isExpanded);
+			} catch {
+				Debug.LogError("Exception");
+				return 0;
+			}
 		}
 
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-			var buttonWidth = EditorGUIUtility.singleLineHeight;
-			//initialize(property);
-			//list.DoList(position);
-			//EditorGUI.LabelField(position, "hola?");
+			try {
+				EditorGUI.BeginProperty(position, GUIContent.none, property);
+				var buttonWidth = EditorGUIUtility.singleLineHeight;
 
-			#region GetPropertyIndex
-			var indexInArrayAsString = property.propertyPath.Substring(
-				property.propertyPath.LastIndexOf("[")).Replace("[","").Replace("]","");
-			var indexInArray = int.Parse(indexInArrayAsString);
-			#endregion
-			var parent = SerializedPropExtension.GetParentProp(property);
+				#region GetPropertyIndex
+				var indexInArray = getIndexInArray(property);
+				#endregion
 
-			position.width -= buttonWidth * 3;
+				var parent = SerializedPropExtension.GetParentProp(property);
 
-			//EditorGUI.PropertyField(position, property);
+				if(indexInArray >= parent.arraySize) Debug.LogError("HERE!!!");
 
-			Rect rect = position;
-			var targetElement = property;
-			bool isExpanded = targetElement.isExpanded;
-			if (isExpanded) rect.width += buttonWidth * 3;
-			rect.height = EditorGUI.GetPropertyHeight(targetElement, GUIContent.none, isExpanded);
-			if (targetElement.hasVisibleChildren)
-				rect.xMin += 10;
-			var propHeader = new GUIContent(targetElement.displayName);
-			EditorGUI.PropertyField(rect, targetElement, propHeader, isExpanded);
+				position.width -= buttonWidth * 3;
 
-			var buttonRect = new Rect(
-				position.x + position.width,
-				position.y,
-				buttonWidth,
-				EditorGUIUtility.singleLineHeight
-			);
+				Rect rect = position;
+				var targetElement = property;
+				bool isExpanded = targetElement.isExpanded;
+				if (isExpanded) rect.width += buttonWidth * 3;
+				rect.height = EditorGUI.GetPropertyHeight(targetElement, GUIContent.none, isExpanded);
+				if (targetElement.hasVisibleChildren)
+					rect.xMin += 10;
+				var propHeader = new GUIContent(targetElement.displayName);
+				EditorGUI.PropertyField(rect, targetElement, propHeader, isExpanded);
 
-			if (indexInArray > 0) {
-				if (GUI.Button(buttonRect, new GUIContent("^", "Move up"), EditorStyles.miniButtonRight)) {
-					parent.MoveArrayElement(indexInArray, indexInArray - 1);
+				var buttonRect = new Rect(position.x + position.width,
+					position.y,
+					buttonWidth,
+					EditorGUIUtility.singleLineHeight);
+
+				if (indexInArray > 0) {
+					if (GUI.Button(buttonRect, new GUIContent("^", "Move up"), EditorStyles.miniButtonRight)) {
+						parent.MoveArrayElement(indexInArray, indexInArray - 1);
+						parent.serializedObject.ApplyModifiedProperties();
+					}
 				}
-			}
 
-			buttonRect.x += buttonWidth;
-			if (indexInArray < parent.arraySize - 1) {
-				if (GUI.Button(buttonRect, new GUIContent("v", "Move down"), EditorStyles.miniButtonRight)) {
-					parent.MoveArrayElement(indexInArray, indexInArray + 1);
+				buttonRect.x += buttonWidth;
+				if (indexInArray < parent.arraySize - 1) {
+					if (GUI.Button(buttonRect, new GUIContent("v", "Move down"), EditorStyles.miniButtonRight)) {
+						parent.MoveArrayElement(indexInArray, indexInArray + 1);
+						parent.serializedObject.ApplyModifiedProperties();
+					}
 				}
-			}
 
-			buttonRect.x += buttonWidth;
-			if (GUI.Button(buttonRect, new GUIContent("x", "Remove item"), EditorStyles.miniButtonRight)) {
-				parent.DeleteArrayElementAtIndex(indexInArray);
+				buttonRect.x += buttonWidth;
+				if (indexInArray < parent.arraySize - 1) { // Because removing the last item throws 2 exceptions in the console.
+					if (GUI.Button(buttonRect, new GUIContent("x", "Remove item"), EditorStyles.miniButtonRight)) {
+						property.DeleteCommand();
+						parent.serializedObject.ApplyModifiedProperties();
+					}
+				}
+
+				EditorGUI.EndProperty();
+			} catch {
+				Debug.LogError("Exception here!");
 			}
 		}
 
-		private void initialize(SerializedProperty property) {
-			if (list != null) return;
-
-			//var target = SerializeableCollectionsPropertyHelper.GetParent(property);
-
-			//property.ParentPath()
-			//fieldInfo.GetValue(target);
-
-			var parent = SerializedPropExtension.GetParentProp(property);
-			//var grandParent = SerializedPropExtension.GetParentProp(parent);
-
-			//Debug.Log($"{parent.name} - {property.name}");
-			//Debug.Log($"{property.propertyPath}");
-
-			list= new ReorderableList(parent.serializedObject, parent, true, false, true, true);
+		private static int getIndexInArray(SerializedProperty property) {
+			var indexInArrayAsString = property.propertyPath.Substring(property.propertyPath.LastIndexOf("["))
+				.Replace("[", "")
+				.Replace("]", "");
+			var indexInArray = int.Parse(indexInArrayAsString);
+			return indexInArray;
 		}
 	}
 }
