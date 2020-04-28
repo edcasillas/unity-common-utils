79709dfe0d279921ea7a0d7c68609fad6530026b
Ed Casillas <eduardo_casillas@hotmail.com>
2020-04-28 15:46:24 -0500

    Continue adding extensions for coroutines.

--- Runtime/Scripts/Extensions/MonoBehaviourExtensions.cs
+++ Runtime/Scripts/Extensions/MonoBehaviourExtensions.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 namespace CommonUtils.Extensions {
 	public static class MonoBehaviourExtensions {
-		public static Coroutine InvokeNextFrame(this MonoBehaviour target, Action action) => target.StartCoroutine(invokeNextFrame(action));
+		public static Coroutine InvokeAfterFrames(this MonoBehaviour target, Action action, int framesToWait) => target.StartCoroutine(invokeAfterFrames(action, framesToWait));
 
-		private static IEnumerator invokeNextFrame(Action action) {
-			yield return null;
+		private static IEnumerator invokeAfterFrames(Action action, int framesToWait) {
+			while(framesToWait-- > 0) yield return null;
+			action.Invoke();
+		}
+
+		public static Coroutine WaitUntil(MonoBehaviour target, Func<bool> condition, Action action) => target.StartCoroutine(waitUntil(condition, action));
+
+		private static IEnumerator waitUntil(Func<bool> condition, Action action) {
+			yield return new WaitUntil(condition);
 			action.Invoke();
 		}
 	}
