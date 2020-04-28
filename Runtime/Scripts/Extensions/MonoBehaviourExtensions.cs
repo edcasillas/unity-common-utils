28408ad684d0c2b4b77314f8bf8dd9282659676c
Ed Casillas <eduardo_casillas@hotmail.com>
2020-04-28 14:46:12 -0500

    Created simple extension to invoke code next frame.

---
+++ Runtime/Scripts/Extensions/MonoBehaviourExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace CommonUtils.Extensions {
+	public static class MonoBehaviourExtensions {
+		public static Coroutine InvokeNextFrame(this MonoBehaviour target, Action action) => target.StartCoroutine(invokeNextFrame(action));
+
+		private static IEnumerator invokeNextFrame(Action action) {
+			yield return new WaitForEndOfFrame();
+			action.Invoke();
+		}
+	}
+}
