dd0059359e7d2717959aedbba4e218d5d0af71c8
Ed Casillas <eduardo_casillas@hotmail.com>
2020-05-07 18:15:31 -0500

    Added simple extension to convert an amount of seconds to a minutes:seconds string.

---
+++ Runtime/Scripts/Extensions/FloatExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CommonUtils.Extensions {
+	public static class FloatExtensions {
+		public static string SecondsToMinutesSecondsString(float seconds) {
+			var t = TimeSpan.FromSeconds(seconds);
+			return $"{t.Minutes:D2}:{t.Seconds:D2}";
+		}
+	}
+}
