192cf9352f99a2ea0063fbaa34869792a6bba27a
Ed Casillas <eduardo_casillas@hotmail.com>
2020-05-11 14:03:07 -0500

    Added  WorldToScreenPointInCanvas extension.

--- Runtime/Scripts/Extensions/Vector3Extensions.cs
+++ Runtime/Scripts/Extensions/Vector3Extensions.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace CommonUtils.Extensions {
 	public static class Vector3Extensions {
@@ -9,9 +11,9 @@ namespace CommonUtils.Extensions {
 		public static Vector3 RandomRange(Vector3 min, Vector3 max) => new Vector3(Random.Range(min.x, max.x),
 																				   Random.Range(min.y, max.y),
 																				   Random.Range(min.z, max.z));
-		
+
 		public static Vector3 RotateAround(this Vector3 point, Vector3 pivot, Quaternion rotation) => rotation * (point - pivot) + pivot;
-		
+
 		/// <summary>
 		/// Translates, rotates and scales the <paramref name="vector"/> by the position, rotation and scale of the transform.
 		/// </summary>
@@ -45,7 +47,41 @@ namespace CommonUtils.Extensions {
 																								 );
 
 		public static bool IsInside(this Vector3 vector, Collider collider) => vector == collider.ClosestPoint(vector);
-		
+
+		/// <summary>
+		/// Transforms a <paramref name="worldPoint"/> seen by <paramref name="worldCamera"/> to a screen point within the specified <paramref name="canvas"/>.
+		/// Supports only <see cref="RenderMode.ScreenSpaceCamera"/> and <see cref="RenderMode.ScreenSpaceOverlay"/> render modes for the <paramref name="canvas"/>.
+		/// Returns <see cref="Vector3.negativeInfinity"/> if the <paramref name="worldPoint"/> is not in front of the <paramref name="worldCamera"/>.
+		/// </summary>
+		/// <param name="worldPoint">Vector3 to be transformed to a screen point.</param>
+		/// <param name="worldCamera">Camera looking at the <paramref name="worldPoint"/>.</param>
+		/// <param name="canvas">Target canvas of the screen point.</param>
+		/// <returns>A Vector3 within the specified <paramref name="canvas"/> that is in the same screen position as the <paramref name="worldPoint"/>.</returns>
+		/// <exception cref="NotImplementedException"><paramref name="canvas"/> has an unsupported RenderMode.</exception>
+		/// <example>
+		/// This sample shows how to call the <see cref="WorldToScreenPointInCanvas"/> method to set the position of a UI image.
+		/// <code>
+		/// var screenPoint = targetObject.transform.position.WorldToScreenPointInCanvas(Camera.main, canvas);
+		/// if(screenPoint != Vector3.negativeInfinity) uiImage.RectTransform.position = screenPoint;
+		/// </code>
+		/// </example>
+		public static Vector3 WorldToScreenPointInCanvas(this Vector3 worldPoint, Camera worldCamera, Canvas canvas) {
+			var direction = worldPoint - worldCamera.transform.position;
+			if (!(Vector3.Dot(worldCamera.transform.forward, direction) > 0.0f)) {
+				return Vector3.negativeInfinity;
+			}
+
+			var screenPoint = worldCamera.WorldToScreenPoint(worldPoint);
+			switch (canvas.renderMode) {
+				case RenderMode.ScreenSpaceCamera:
+					return MathUtils.ScreenPointToLocalPointInRectangle(canvas, position: screenPoint);
+				case RenderMode.ScreenSpaceOverlay:
+					return screenPoint;
+				default:
+					throw new NotImplementedException("RenderMode not Supported.");
+			}
+		}
+
 		public static string ToStringVerbose(this Vector3 v) => $"({v.x}, {v.y}, {v.z})";
 	}
 }
