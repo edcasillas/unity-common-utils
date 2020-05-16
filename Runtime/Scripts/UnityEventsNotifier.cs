d284cbdb93c83da1f20eeca004c971f793e6a033
Ed <eduardo_casillas@hotmail.com>
2020-05-15 20:06:38 -0500

    Included methods to subscribe/unsubscribe to unity events notifier from code.

--- Runtime/Scripts/UnityEventsNotifier.cs
+++ Runtime/Scripts/UnityEventsNotifier.cs
@@ -16,6 +16,7 @@ namespace CommonUtils {
 		[SerializeField] private UnityEvent onDestroy;
 #pragma warning restore 649
 
+		#region Unity Lifecycle
 		private void Awake() => awake.Invoke();
 
 		private void Start() => start.Invoke();
@@ -29,5 +30,23 @@ namespace CommonUtils {
 		private void OnBecameVisible() => onBecameVisible.Invoke();
 
 		private void OnBecameInvisible() => onBecameInvisible.Invoke();
+		#endregion
+
+		#region Public Methods
+		public void SubscribeAwake(UnityAction call) => awake.AddListener(call);
+		public void UnsubscribeAwake(UnityAction call) => awake.RemoveListener(call);
+		public void SubscribeOnEnable(UnityAction call) => onEnable.AddListener(call);
+		public void UnsubscribeOnEnable(UnityAction call) => onEnable.RemoveListener(call);
+		public void SubscribeStart(UnityAction call) => start.AddListener(call);
+		public void UnsubscribeStart(UnityAction call) => start.RemoveListener(call);
+		public void SubscribeOnDisable(UnityAction call) => onDisable.AddListener(call);
+		public void UnsubscribeOnDisable(UnityAction call) => onDisable.RemoveListener(call);
+		public void SubscribeOnDestroy(UnityAction call) => onDestroy.AddListener(call);
+		public void UnsubscribeOnDestroy(UnityAction call) => onDestroy.RemoveListener(call);
+		public void SubscribeOnBecameVisible(UnityAction call) => onBecameInvisible.AddListener(call);
+		public void UnsubscribeOnBecameVisible(UnityAction call) => onBecameInvisible.RemoveListener(call);
+		public void SubscribeOnBecameInvisible(UnityAction call) => onBecameInvisible.AddListener(call);
+		public void UnsubscribeOnBecameInvisible(UnityAction call) => onBecameInvisible.RemoveListener(call);
+		#endregion
 	}
 }
