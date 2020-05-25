6d7dc64d36f136034a1711f3ad3627fd280ba713
Ed Casillas <eduardo_casillas@hotmail.com>
2020-05-25 15:51:24 -0500

    Tweaking UI spinner.

---
+++ Runtime/Scripts/UI/UISpinner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace CommonUtils.UI {
+	[AddComponentMenu("UI/UI Spinner")]
+	[RequireComponent(typeof(RectTransform))]
+	public class UISpinner : MonoBehaviour {
+		#pragma warning disable 649
+		[SerializeField] private float speed = 300f;
+		#pragma warning restore 649
+
+		private RectTransform tr;
+
+		private void Awake() => tr = GetComponent<RectTransform>();
+
+		private void Update() => tr.Rotate(0,0,speed * Time.deltaTime);
+	}
+}
