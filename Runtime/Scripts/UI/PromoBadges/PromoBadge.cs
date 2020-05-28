142e682d469b99cb5b32d0bbe1f8a72fa6939107
Ed <eduardo_casillas@hotmail.com>
2020-05-28 13:56:27 -0500

    Extending the PromoBadge to use a resource from a server.

--- Runtime/Scripts/UI/PromoBadges/PromoBadge.cs
+++ Runtime/Scripts/UI/PromoBadges/PromoBadge.cs
@@ -1,4 +1,3 @@
-using System;
 using CommonUtils.Extensions;
 using CommonUtils.WebResources;
 using UnityEngine;
@@ -12,28 +11,17 @@ namespace CommonUtils.UI.PromoBadges {
 	[AddComponentMenu("UI/Promo Badge")]
 	[RequireComponent(typeof(Button))]
 	public class PromoBadge : MonoBehaviour {
-		[Serializable]
-		public class ApplicationData {
-			public Sprite SourceImage;
-			[FormerlySerializedAs("URL")]
-			[Tooltip("URL to open on non-mobile platforms, and used as a fallback when platform-specific URLs have not been specified.")]
-			public string FallbackUrl;
-			public StringPerPlatformDictionary UrlPerPlatform;
-		}
-
 		#region Inspector fields
-		#pragma warning disable 649
+#pragma warning disable 649
 		[SerializeField] private GameObject badgeContainer;
 		[SerializeField] private GameObject loadingOverlay;
-		[FormerlySerializedAs("BadgeImage")]
 		[SerializeField] private Image badgeImage;
 		[SerializeField] private string remoteConfigUrl;
 		[SerializeField] private AppDataCollection testConfig;
-		public ApplicationData[] Apps;
-		#pragma warning restore 649
+#pragma warning restore 649
 		#endregion
 		
-		private ApplicationData appShown;
+		private string targetUrl;
 
 		private Button _button;
 
@@ -51,33 +39,29 @@ namespace CommonUtils.UI.PromoBadges {
 		}
 
 		private void Start() {
-			if(Apps == null || Apps.Length == 0) {
+			if(testConfig?.Apps == null || testConfig.Apps.Length == 0) {
 				Debug.LogWarning($"{nameof(PromoBadge)} '{name}' doesn't have any apps set.", this);
 				gameObject.SetActive(false);
 				return;
 			}
 
 			var newAppShown = testConfig.Apps.PickRandom();
+			targetUrl = newAppShown.UrlPerPlatform.ContainsKey(Application.platform) ? newAppShown.UrlPerPlatform[Application.platform] : newAppShown.FallbackUrl;
+			if (string.IsNullOrEmpty(targetUrl)) {
+				gameObject.SetActive(false);
+				return;
+			}
+
 			WebLoader.LoadWebTexture(newAppShown.ImageUrl,
 				response => {
 					loadingOverlay.SetActive(false);
 					if (!response.IsSuccess || !(response.Data is Texture2D tex)) return;
-					appShown = new ApplicationData {FallbackUrl = newAppShown.FallbackUrl, UrlPerPlatform = newAppShown.UrlPerPlatform};
 					badgeImage.sprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);
 					badgeContainer.SetActive(true);
 					button.interactable = true;
 				});
 		}
 
-		public void Go() {
-			var realUrl = appShown.UrlPerPlatform.ContainsKey(Application.platform) ? appShown.UrlPerPlatform[Application.platform] : appShown.FallbackUrl;
-
-			if (string.IsNullOrWhiteSpace(realUrl)) {
-				Debug.LogError("The current application doesn't have a URL defined for this platform and no fallback URL has been defined.", this);
-				return;
-			}
-			
-			Application.OpenURL(realUrl);
-		}
+		public void Go() => Application.OpenURL(targetUrl);
 	}
 }
