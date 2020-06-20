a4e9bda9c6793870daf1a9dec4d6a66740cb3f01
Ed Casillas <eduardo_casillas@hotmail.com>
2020-06-19 21:05:50 -0500

    Added animator extensions class.

---
+++ Runtime/Scripts/Extensions/AnimatorExtensions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace CommonUtils.Extensions {
+	public static class AnimatorExtensions {
+		public static bool HasParameter(this Animator animator, string name) {
+			var allParameters = animator.parameters;
+			foreach (var param in allParameters) {
+				if (param.name == name) return true;
+			}
+
+			return false;
+		}
+
+		public static bool HasParameter(this Animator animator, int nameHash) {
+			var allParameters = animator.parameters;
+			foreach (var param in allParameters) {
+				if (param.nameHash == nameHash) return true;
+			}
+
+			return false;
+		}
+	}
+}
