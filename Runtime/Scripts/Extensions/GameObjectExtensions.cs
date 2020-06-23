6106ceaeec498b978bf9033d3b4586e23629b563
Ed Casillas <eduardo_casillas@hotmail.com>
2020-06-23 13:10:46 -0500

    Added GameObject extensions.

---
+++ Runtime/Scripts/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace CommonUtils.Extensions {
+	public static class GameObjectExtensions {
+		public static GameObject[] GetChildren(this GameObject gameObject) {
+			var result = new GameObject[gameObject.transform.childCount];
+			var i = 0;
+			foreach (Transform child in gameObject.transform) {
+				if (child == gameObject.transform) continue;
+				result[i++] = child.gameObject;
+			}
+
+			return result;
+		}
+	}
+}
