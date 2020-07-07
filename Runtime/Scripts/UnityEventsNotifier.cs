4c3e052115071a81919fa59cec136010b20c8116
Ed Casillas <eduardo_casillas@hotmail.com>
2020-07-07 14:03:27 -0500

    Fixed issues in the UnityEventsNotifier and added a unit test for it.

--- Runtime/Scripts/UnityEventsNotifier.cs
+++ Runtime/Scripts/UnityEventsNotifier.cs
@@ -17,36 +17,76 @@ namespace CommonUtils {
 #pragma warning restore 649
 
 		#region Unity Lifecycle
-		private void Awake() => awake.Invoke();
+		private void Awake() => awake?.Invoke();
 
-		private void Start() => start.Invoke();
+		private void Start() => start?.Invoke();
 
-		private void OnEnable() => onEnable.Invoke();
+		private void OnEnable() => onEnable?.Invoke();
 
-		private void OnDisable() => onDisable.Invoke();
+		private void OnDisable() => onDisable?.Invoke();
 
-		private void OnDestroy() => onDestroy.Invoke();
+		private void OnDestroy() {
+			onDestroy?.Invoke();
+			UnsubscribeAll();
+		}
 
-		private void OnBecameVisible() => onBecameVisible.Invoke();
+		private void OnBecameVisible() => onBecameVisible?.Invoke();
 
-		private void OnBecameInvisible() => onBecameInvisible.Invoke();
+		private void OnBecameInvisible() => onBecameInvisible?.Invoke();
 		#endregion
 
 		#region Public Methods
-		public void SubscribeAwake(UnityAction call) => awake.AddListener(call);
-		public void UnsubscribeAwake(UnityAction call) => awake.RemoveListener(call);
-		public void SubscribeOnEnable(UnityAction call) => onEnable.AddListener(call);
-		public void UnsubscribeOnEnable(UnityAction call) => onEnable.RemoveListener(call);
-		public void SubscribeStart(UnityAction call) => start.AddListener(call);
-		public void UnsubscribeStart(UnityAction call) => start.RemoveListener(call);
-		public void SubscribeOnDisable(UnityAction call) => onDisable.AddListener(call);
-		public void UnsubscribeOnDisable(UnityAction call) => onDisable.RemoveListener(call);
-		public void SubscribeOnDestroy(UnityAction call) => onDestroy.AddListener(call);
-		public void UnsubscribeOnDestroy(UnityAction call) => onDestroy.RemoveListener(call);
-		public void SubscribeOnBecameVisible(UnityAction call) => onBecameInvisible.AddListener(call);
-		public void UnsubscribeOnBecameVisible(UnityAction call) => onBecameInvisible.RemoveListener(call);
-		public void SubscribeOnBecameInvisible(UnityAction call) => onBecameInvisible.AddListener(call);
-		public void UnsubscribeOnBecameInvisible(UnityAction call) => onBecameInvisible.RemoveListener(call);
+		public void SubscribeAwake(UnityAction call) {
+			if (awake == null) awake = new UnityEvent();
+			awake.AddListener(call);
+		}
+		public void UnsubscribeAwake(UnityAction call) => awake?.RemoveListener(call);
+
+		public void SubscribeOnEnable(UnityAction call) {
+			if (onEnable == null) onEnable = new UnityEvent();
+			onEnable.AddListener(call);
+		}
+		public void UnsubscribeOnEnable(UnityAction call) => onEnable?.RemoveListener(call);
+
+		public void SubscribeStart(UnityAction call) {
+			if(start == null) start = new UnityEvent();
+			start.AddListener(call);
+		}
+		public void UnsubscribeStart(UnityAction call) => start?.RemoveListener(call);
+
+		public void SubscribeOnDisable(UnityAction call) {
+			if(onDisable == null) onDisable = new UnityEvent();
+			onDisable.AddListener(call);
+		}
+		public void UnsubscribeOnDisable(UnityAction call) => onDisable?.RemoveListener(call);
+
+		public void SubscribeOnDestroy(UnityAction call) {
+			if (onDestroy == null) onDestroy = new UnityEvent();
+			onDestroy.AddListener(call);
+		}
+		public void UnsubscribeOnDestroy(UnityAction call) => onDestroy?.RemoveListener(call);
+
+		public void SubscribeOnBecameVisible(UnityAction call) {
+			if (onBecameVisible == null) onBecameVisible = new UnityEvent();
+			onBecameVisible.AddListener(call);
+		}
+		public void UnsubscribeOnBecameVisible(UnityAction call) => onBecameInvisible?.RemoveListener(call);
+
+		public void SubscribeOnBecameInvisible(UnityAction call) {
+			if(onBecameInvisible == null) onBecameInvisible = new UnityEvent();
+			onBecameInvisible.AddListener(call);
+		}
+		public void UnsubscribeOnBecameInvisible(UnityAction call) => onBecameInvisible?.RemoveListener(call);
+
+		public void UnsubscribeAll() {
+			awake?.RemoveAllListeners();
+			onEnable?.RemoveAllListeners();
+			start?.RemoveAllListeners();
+			onDisable?.RemoveAllListeners();
+			onDestroy?.RemoveAllListeners();
+			onBecameVisible?.RemoveAllListeners();
+			onBecameInvisible?.RemoveAllListeners();
+		}
 		#endregion
 	}
 }
---
+++ Tests/PlayMode/UnityEventsNotifierTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.TestTools;
+
+namespace CommonUtils.Tests.PlayMode {
+	public class UnityEventsNotifierTests {
+		[UnityTest]
+		public IEnumerator SubscribeIsSuccessful_WhenAddingComponentAtRuntime() {
+			// Arrange
+			var gameObject = Object.Instantiate(new GameObject());
+			var success = false;
+
+			// Act
+			var eventsNotifier = gameObject.AddComponent<UnityEventsNotifier>();
+			eventsNotifier.SubscribeOnDestroy(() => success = true);
+			yield return null;
+			Object.Destroy(gameObject);
+			yield return null;
+
+			// Assert
+			Assert.IsTrue(success);
+		}
+	}
+}
