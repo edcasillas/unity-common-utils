1bcffecb043920bfe0b6a2f8cf959bc640117c47
Ed Casillas <eduardo_casillas@hotmail.com>
2020-07-15 04:04:45 -0500

    Improved the ObjectFinder to search in all open scenes.

--- Runtime/Scripts/ObjectFinder.cs
+++ Runtime/Scripts/ObjectFinder.cs
@@ -10,9 +10,15 @@ namespace CommonUtils {
 		/// <param name="includeInactive">Should Components on inactive GameObjects be included in the found set?</param>
 		/// <typeparam name="T">The type of Component to retrieve.</typeparam>
 		/// <returns>Collection of components of type <typeparamref name="T"/></returns>
-		public static IEnumerable<T> FindObjectsOfType<T>(bool includeInactive = false) =>
-			SceneManager.GetActiveScene()
-				.GetRootGameObjects()
-				.SelectMany(go => go.GetComponentsInChildren<T>(includeInactive));
+		public static IEnumerable<T> FindObjectsOfType<T>(bool includeInactive = false) {
+			var result = Enumerable.Empty<T>();
+			for (int index = 0; index < SceneManager.sceneCount; ++index) {
+				result = result.Concat(SceneManager.GetSceneAt(index)
+					.GetRootGameObjects()
+					.SelectMany(go => go.GetComponentsInChildren<T>(includeInactive)));
+			}
+
+			return result;
+		}
 	}
 }
