cd758024c9672021bffd1b34093f09c8b415bff1
Ed Casillas <eduardo_casillas@hotmail.com>
2020-09-15 00:19:06 -0500

    Added BehaviourExtensions.

---
+++ Runtime/Scripts/Extensions/BehaviourExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommonUtils.Extensions {
+	/// <summary>
+	/// Extensions for components that can be enabled or disabled.
+	/// </summary>
+	public static class BehaviourExtensions {
+		/// <summary>
+		/// Enables or disables all <paramref name="behaviours"/>.
+		/// </summary>
+		public static void SetEnabled(this IEnumerable<Behaviour> behaviours, bool enable) {
+			if(behaviours.IsNullOrEmpty()) return;
+			foreach (var behaviour in behaviours) {
+				behaviour.enabled = enable;
+			}
+		}
+	}
+}
