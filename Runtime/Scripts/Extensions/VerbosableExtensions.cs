2a6645a8aac09ad4623fa50afa4cb06a21c73ef5
Ed <eduardo_casillas@hotmail.com>
2021-04-26 12:13:14 -0700

    Added extension to use verbosables without GameObjects

--- Runtime/Scripts/Extensions/VerbosableExtensions.cs
+++ Runtime/Scripts/Extensions/VerbosableExtensions.cs
@@ -15,7 +15,20 @@ namespace CommonUtils.Extensions {
 			where TVerbosable : Object, IVerbosable {
 			if (verbosable.IsVerbose) Debug.Log(message, verbosable);
 		}
-		
+
+		/// <summary>
+		/// Writes a <paramref name="message"/> to the console if the <paramref name="verbosable"/> has its <see cref="IVerbosable.IsVerbose"/> property set to true.
+		/// </summary>
+		/// <param name="verbosable">Verbosable component writing the message.</param>
+		/// <param name="message">Message to be sent to the console.</param>
+		/// <typeparam name="TVerbosable">Type of verbosable component.</typeparam>
+		/// <remarks>Use this variant when you don't want the log message to be linked to the GameObject context, or
+		/// when the verbosable object is not an <see cref="IUnityComponent"/>.</remarks>
+		public static void DebugLogNoContext<TVerbosable>(this TVerbosable verbosable, string message)
+			where TVerbosable : IVerbosable {
+			if (verbosable.IsVerbose) Debug.Log(message);
+		}
+
 		/// <summary>
 		/// Writes a <paramref name="message"/> to the console if the <paramref name="verbosable"/> has its <see cref="IVerbosable.IsVerbose"/> property set to true.
 		/// </summary>
@@ -50,7 +63,7 @@ namespace CommonUtils.Extensions {
 			where TVerbosable : Object, IVerbosable {
 			if (verbosable.IsVerbose) Debug.Log(messageDelegate.Invoke(), verbosable);
 		}
-		
+
 		/// <summary>
 		/// Writes a message to the console if the <paramref name="verbosable"/> has its <see cref="IVerbosable.IsVerbose"/> property set to true.
 		/// The <paramref name="messageDelegate"/> is only invoked when the message is effectively being sent to the console, so this variant of DebugLog is meant to
