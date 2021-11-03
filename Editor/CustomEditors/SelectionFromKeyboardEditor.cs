8d50d5a9a75ec54508d620ee77d5f91e4508d87b
Ed <eduardo_casillas@hotmail.com>
2021-11-03 00:32:17 -0700

    Creating editor options for SelectionFromKeyboard

--- Editor/CustomEditors/SelectionFromKeyboardEditor.cs
+++ Editor/CustomEditors/SelectionFromKeyboardEditor.cs
@@ -1,17 +1,48 @@
+using CommonUtils.Extensions;
 using UnityEditor;
 using UnityEngine;
 
 namespace CommonUtils.Editor.CustomEditors {
     [CustomEditor(typeof(SelectionFromKeyboard))]
     public class SelectionFromKeyboardEditor : UnityEditor.Editor {
+        #region Constants
+        private const string EditorPrefKey_ShowInSceneView = nameof(CommonUtils) + "." + nameof(SelectionFromKeyboardEditor) + "." + nameof(showInSceneView);
+        private const string EditorPrefKey_RectFillColor = nameof(CommonUtils) + "." + nameof(SelectionFromKeyboardEditor) +"." + nameof(rectFillColor);
+        #endregion
+        
+        #region Properties
+        private static bool showInSceneView {
+            get => EditorPrefs.GetBool(EditorPrefKey_ShowInSceneView, true);
+            set => EditorPrefs.SetBool(EditorPrefKey_ShowInSceneView, value);
+        }
+        
+        private static Color rectFillColor {
+            get {
+                var hex = EditorPrefs.GetString(EditorPrefKey_RectFillColor, Color.white.ColorToHex());
+                return hex.HexToColor();
+            }
+            set => EditorPrefs.SetString(EditorPrefKey_RectFillColor, value.ColorToHex(true));
+        }
+
+        #endregion
+        
         private SelectionFromKeyboard subject;
+        private bool showDisplayOptions;
 
         private void OnEnable() {
             subject = (SelectionFromKeyboard) target;
         }
 
+        public override void OnInspectorGUI() {
+            showDisplayOptions = EditorExtensions.Collapse(showDisplayOptions, "Display Options", () => {
+                showInSceneView = EditorGUILayout.Toggle("Show Children Indices in Scene View", showInSceneView);
+                rectFillColor = EditorGUILayout.ColorField("Rect Fill Color", rectFillColor);
+            });
+            DrawDefaultInspector();
+        }
+
         private void OnSceneGUI() {
-            if(Event.current.type != EventType.Repaint) return;
+            if(!showInSceneView || Event.current.type != EventType.Repaint) return;
             var style = new GUIStyle {normal = {textColor = Color.black}, alignment = TextAnchor.MiddleCenter};
 
             const float rectSize = 25;
@@ -21,7 +52,7 @@ namespace CommonUtils.Editor.CustomEditors {
             foreach (var child in subject.childrenToSelect) {
                 if (child.gameObject.activeInHierarchy) {
                     var bgRectCenter = (Vector2)child.transform.position - halfSizeVector - offset;
-                    Handles.DrawSolidRectangleWithOutline(new Rect(bgRectCenter, Vector2.one * rectSize), Color.white, Color.black);
+                    Handles.DrawSolidRectangleWithOutline(new Rect(bgRectCenter, Vector2.one * rectSize), rectFillColor, Color.black);
                     Handles.Label(child.transform.position, (i++).ToString(), style);
                 }
             }
--- Runtime/Scripts/Extensions/ColorExtensions.cs
+++ Runtime/Scripts/Extensions/ColorExtensions.cs
@@ -3,18 +3,25 @@ using UnityEngine;
 
 namespace CommonUtils.Extensions {
 	public static class ColorExtensions {
-		public static string ColorToHex(this Color color)
-			=> color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
+		public static string ColorToHex(this Color color, bool includeAlpha = false) {
+			Color32 color32 = color;
+			var result = color32.r.ToString ("X2") + color32.g.ToString ("X2") + color32.b.ToString ("X2");
+			if (includeAlpha) result += color32.a.ToString("X2");
+			return result;
+		}
 
 		public static Color HexToColor(this string inputHexString) {
 			if (string.IsNullOrEmpty(inputHexString)) throw new ArgumentNullException(nameof(inputHexString));
-			if (inputHexString.Length != 6)
-				throw new ArgumentException("Input string must have exactly 6 characters.", nameof(inputHexString));
+			if (inputHexString.Length != 6 && inputHexString.Length != 8)
+				throw new ArgumentException("Input string must have exactly 6 or 8 characters (without or with alpha).", nameof(inputHexString));
 
 			var r = byte.Parse(inputHexString.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
 			var g = byte.Parse(inputHexString.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
 			var b = byte.Parse(inputHexString.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
-			return new Color32(r, g, b, 255);
+			var a = inputHexString.Length == 8
+				? byte.Parse(inputHexString.Substring(6, 2), System.Globalization.NumberStyles.HexNumber)
+				: (byte)255;
+			return new Color32(r, g, b, a);
 		}
 		
 		public static Color GetColorFromRGB255(int r, int g, int b) => new Color (r / 255.0f, g / 255.0f, b / 255.0f);
