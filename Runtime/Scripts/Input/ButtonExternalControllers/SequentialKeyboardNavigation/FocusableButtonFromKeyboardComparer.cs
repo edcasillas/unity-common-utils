4885ddf324481c7400a904e7a7a70bccfc05a17f
Ed <eduardo_casillas@hotmail.com>
2021-11-04 12:09:39 -0700

    Fixed an issue with TabIndex comparisons in sequential navigation.

--- Runtime/Scripts/Input/ButtonExternalControllers/SequentialKeyboardNavigation/FocusableButtonFromKeyboardComparer.cs
+++ Runtime/Scripts/Input/ButtonExternalControllers/SequentialKeyboardNavigation/FocusableButtonFromKeyboardComparer.cs
@@ -8,7 +8,11 @@ namespace CommonUtils.Input.ButtonExternalControllers.SequentialKeyboardNavigati
             if (!x.IsValid() && !y.IsValid()) return 0;
             if (!x.IsValid()) return 1;
             if (!y.IsValid()) return -1;
-            return x.TabIndex.CompareTo(y.TabIndex);
+            var result = x.TabIndex.CompareTo(y.TabIndex);
+            
+            // If the TabIndex is the same in both items, treat the first one as smaller; otherwise returning zero
+            // will provoke the second item not to be included in the SortedSet.
+            return result == 0 ? -1 : x.TabIndex.CompareTo(y.TabIndex);
         }
     }
 }
