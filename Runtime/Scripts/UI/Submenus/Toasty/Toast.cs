b1919d48beed8f4f0f2891fc13f59df806f61110
Ed Casillas <eduardo_casillas@hotmail.com>
2022-12-04 12:11:31 -0800

    Progress writing toasts

--- Runtime/Scripts/UI/Submenus/Toasty/Toast.cs
+++ Runtime/Scripts/UI/Submenus/Toasty/Toast.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
 
-namespace CommonUtils.UI.Submenus.Toasty
-{
-    public class Toast : AbstractSubmenu
-    {
+namespace CommonUtils.UI.Submenus.Toasty {
+	[AddComponentMenu("UI/Animated Submenu/Toast")]
+	public class Toast : AbstractSubmenu {
+		[ShowInInspector] public Vector2 position { get; set; }
+
 		protected override void OnInit() {
+			float rectWidth = (RectTransform.anchorMax.x - RectTransform.anchorMin.x)*Screen.width;
+			float rectHeight =(RectTransform.anchorMax.y - RectTransform.anchorMin.y)*Screen.height;
+			 position = new Vector2(RectTransform.anchorMin.x*Screen.width, RectTransform.anchorMin.y * Screen.height);
+
 			// For now we are only concerned about the height of the toast, assuming the width will be controlled by the manager.
 			HiddenValue = Vector2.zero;
-			ShownValue = new Vector2(0, RectTransform.rect.height);
+			ShownValue = RectTransform.sizeDelta;
 		}
 
-		public override void OnAnimationUpdated(Vector2 updatedValue) => throw new System.NotImplementedException();
+		public override void OnAnimationUpdated(Vector2 updatedValue)
+			=> RectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, updatedValue.y);
 	}
 }
