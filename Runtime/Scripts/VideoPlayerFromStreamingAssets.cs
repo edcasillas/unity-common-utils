40d3ad0d5eaab9c8b6b1d1ee1f4fcf76d08ba7fe
Ed <eduardo_casillas@hotmail.com>
2022-12-15 17:34:01 -0800

    Fixed the play and pause methods of the video player from streaming assets

--- Runtime/Scripts/VideoPlayerFromStreamingAssets.cs
+++ Runtime/Scripts/VideoPlayerFromStreamingAssets.cs
@@ -8,37 +8,42 @@ namespace CommonUtils {
 	/// A component to load a video from the Streaming Assets folder, based on Unity's documentation.
 	/// </summary>
 	[RequireComponent(typeof(VideoPlayer))]
-	public class VideoPlayerFromStreamingAssets : MonoBehaviour, IVerbosable {
-		[SerializeField] private string fileName; // eg myFile.mp4
-		[SerializeField] private bool verbose;
-
-		public bool IsVerbose => verbose;
+	public class VideoPlayerFromStreamingAssets : EnhancedMonoBehaviour {
+		[Tooltip("Example: myFile.mp4")]
+		[SerializeField] private string fileName;
+		[SerializeField] private bool autoPlay = true;
 
 		private VideoPlayer videoPlayer;
 		private string status;
 
-		private void Start() {
+		#region Unity Lifecycle
+		private void Awake() {
 			videoPlayer = GetComponent<VideoPlayer>();
-
 			// Obtain the location of the video clip.
 			videoPlayer.url = Path.Combine(Application.streamingAssetsPath, fileName);
+		}
 
-			this.DebugLog($"Trying to run video from {videoPlayer.url}");
+		private void Start() {
+			if(!autoPlay) return;
+			this.DebugLog(() => $"Trying to run video from {videoPlayer.url}");
+			Play();
+		}
+		#endregion
 
+		#region Play/Pause
+		[ShowInInspector]
+		public void Play() {
+			if(videoPlayer.isPlaying) return;
 			// Restart from beginning when done.
 			videoPlayer.isLooping = true;
-
 			videoPlayer.Play();
 		}
 
-		public void Play() {
-			if(!videoPlayer.isPaused) return;
-			videoPlayer.Pause();
-		}
-
+		[ShowInInspector]
 		public void Pause() {
 			if(videoPlayer.isPaused) return;
-			videoPlayer.Play();
+			videoPlayer.Pause();
 		}
+		#endregion
 	}
 }
