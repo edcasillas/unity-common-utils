0c67f218fd218a3a69bf5ee708ef77bb6edad25e
Ed <eduardo_casillas@hotmail.com>
2023-01-16 11:53:03 -0800

    Added constructors to randomizable classes to allow them to have default values set in the inspector.

--- Runtime/Scripts/Serializables/Randomizables/RandomInt.cs
+++ Runtime/Scripts/Serializables/Randomizables/RandomInt.cs
@@ -3,6 +3,9 @@
 namespace CommonUtils.Serializables.Randomizables {
 	[Serializable]
 	public class RandomInt : AbstractRandomizable<int> {
+		public RandomInt() {}
+		public RandomInt(int defaultMin, int defaultMax) : base(defaultMin, defaultMax) { }
+
 		public override int Get() => UnityEngine.Random.Range(Min, Max + 1);
 	}
 }
