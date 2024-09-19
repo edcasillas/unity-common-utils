a9985f9a8c474bb7a0bfdb9a6af0495fed96fab2
Ed Casillas <ecasillasmarca@zynga.com>
2024-09-19 16:08:51 -0700

    Redesigning SceneLoader

--- Runtime/Scripts/ISceneLoaderImpl.cs
+++ Runtime/Scripts/ISceneLoaderImpl.cs
@@ -22,14 +22,26 @@ namespace CommonUtils
 
 		public void LoadScene(int sceneIndex) => SceneManager.LoadScene(sceneIndex);
 		public void LoadScene(int sceneIndex, Action<AsyncOperation> onReadyToActivate) => Coroutiner.StartCoroutine(doLoad(sceneIndex, onReadyToActivate));
-
-		public void LoadScene(string scenePath) => throw new NotImplementedException();
-		public void LoadScene(string scenePath, Action<AsyncOperation> onReadyToActivate) => throw new NotImplementedException();
+		public void LoadScene(string scenePath) => SceneManager.LoadScene(scenePath);
+		public void LoadScene(string scenePath, Action<AsyncOperation> onReadyToActivate) => Coroutiner.StartCoroutine(doLoad(scenePath, onReadyToActivate));
 
 		private IEnumerator doLoad(int sceneIndex, Action<AsyncOperation> onReadyToActivate) {
 			var asyncLoad = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Single);
 			if (asyncLoad == null) {
 				this.LogNoContext($"Could not load scene with build index {sceneIndex}. SceneManager.LoadSceneAsync failed. Aborting.");
+				yield break;
+			}
+			asyncLoad.allowSceneActivation = false;
+			while(asyncLoad.progress < 0.9f) {
+				yield return null;
+			}
+			onReadyToActivate(asyncLoad);
+		}
+
+		private IEnumerator doLoad(string scenePath, Action<AsyncOperation> onReadyToActivate) {
+			var asyncLoad = SceneManager.LoadSceneAsync(scenePath, LoadSceneMode.Single);
+			if (asyncLoad == null) {
+				this.LogNoContext($"Could not load scene with path \"{scenePath}\". SceneManager.LoadSceneAsync failed. Aborting.");
 				yield break;
 			}
 			asyncLoad.allowSceneActivation = false;
