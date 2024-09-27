1f557337ca474b7fc93a0b585468c11c34e6f621
Ed Casillas <eduardo_casillas@hotmail.com>
2024-09-27 16:46:48 -0700

    Added comments

--- Runtime/Scripts/UI/SettingsFields/PlayerPrefsSettingsFields/AbstractPlayerPrefsEnumDropdownFieldController.cs
+++ Runtime/Scripts/UI/SettingsFields/PlayerPrefsSettingsFields/AbstractPlayerPrefsEnumDropdownFieldController.cs
@@ -3,12 +3,16 @@ using CommonUtils.Extensions;
 using UnityEngine;
 
 namespace CommonUtils.UI.SettingsFields.PlayerPrefsSettingsFields {
+	/// <summary>
+	/// Controls a drop down field that is connected to a PlayerPrefs key storing an enum value.
+	/// To use it, create a subclass to specify the type of enum, then attach the inherited class to a game object
+	/// with a Dropdown component.
+	/// </summary>
+	/// <typeparam name="TEnum">Type of enum handled by this drop down list.</typeparam>
 	public class AbstractPlayerPrefsEnumDropdownFieldController<TEnum> : AbstractEnumDropdownFieldController<TEnum>
 		where TEnum : Enum {
-#pragma warning disable 649
 		[SerializeField] private string playerPrefsKey;
 		[SerializeField] private TEnum defaultValue;
-#pragma warning restore 649
 
 		protected override TEnum DefaultValue => PlayerPrefs.GetInt(playerPrefsKey, defaultValue.ToInt()).ToEnumValue<TEnum>();
 
