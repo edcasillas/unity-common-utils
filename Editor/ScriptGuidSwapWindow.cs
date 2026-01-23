0597f011e721ac31037da9f6e79924e00f7047dc
Ed Casillas <ecasillasmarca@zynga.com>
2026-01-23 15:10:45 -0800

    Trying a different approach to replace components by swapping GUIDs

---
+++ Editor/ScriptGuidSwapWindow.cs
@@ -0,0 +1,66 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace CommonUtils.Editor {
+	public class ScriptGuidSwapWindow : EditorWindow {
+		private MonoScript legacyScript;
+		private MonoScript newScript;
+		private string statusMessage = "";
+
+		[MenuItem("Tools/Script GUID Swap")]
+		public static void ShowWindow() => GetWindow<ScriptGuidSwapWindow>("Script GUID Swap");
+
+		private void OnGUI() {
+			EditorGUILayout.LabelField("Script GUID Swap", EditorStyles.boldLabel);
+			EditorGUILayout.HelpBox("Swaps GUIDs in .meta files. All references to legacy script will point to new script.", MessageType.Info);
+			EditorGUILayout.Space();
+
+			legacyScript = (MonoScript)EditorGUILayout.ObjectField("Legacy Script", legacyScript, typeof(MonoScript), false);
+			newScript = (MonoScript)EditorGUILayout.ObjectField("New Script", newScript, typeof(MonoScript), false);
+
+			EditorGUILayout.Space();
+
+			GUI.enabled = legacyScript != null && newScript != null;
+			if (GUILayout.Button("Swap GUIDs", GUILayout.Height(30)))
+				swapGuids();
+			GUI.enabled = true;
+
+			EditorGUILayout.Space();
+			EditorGUILayout.TextArea(statusMessage, GUILayout.ExpandHeight(true));
+		}
+
+		private void swapGuids() {
+			if (!EditorUtility.DisplayDialog("Swap Script GUIDs",
+				$"Swap GUIDs between '{legacyScript.name}' and '{newScript.name}'?\n\nAll references to legacy script will point to new script.",
+				"Yes", "Cancel"))
+				return;
+
+			var legacyPath = AssetDatabase.GetAssetPath(legacyScript);
+			var newPath = AssetDatabase.GetAssetPath(newScript);
+			var legacyMetaPath = legacyPath + ".meta";
+			var newMetaPath = newPath + ".meta";
+
+			if (!File.Exists(legacyMetaPath) || !File.Exists(newMetaPath)) {
+				statusMessage = "Error: Meta files not found.";
+				return;
+			}
+
+			var legacyGuid = AssetDatabase.AssetPathToGUID(legacyPath);
+			var newGuid = AssetDatabase.AssetPathToGUID(newPath);
+
+			var legacyMeta = File.ReadAllText(legacyMetaPath);
+			var newMeta = File.ReadAllText(newMetaPath);
+
+			legacyMeta = legacyMeta.Replace($"guid: {legacyGuid}", $"guid: {newGuid}");
+			newMeta = newMeta.Replace($"guid: {newGuid}", $"guid: {legacyGuid}");
+
+			File.WriteAllText(legacyMetaPath, legacyMeta);
+			File.WriteAllText(newMetaPath, newMeta);
+
+			AssetDatabase.Refresh();
+
+			statusMessage = $"Success!\n\nSwapped GUIDs:\n{legacyScript.name}: {legacyGuid} ↔ {newGuid}\n{newScript.name}: {newGuid} ↔ {legacyGuid}\n\nAll references now point to new script.";
+		}
+	}
+}
